3cf5aafb7e64e3cdf7b932052f927aad2c99c2b0
Maxim <maxim.milisov@gmail.com>
2021-12-23 01:14:55 +0300

    правка подключаемых библотек

--- MatrixDeterminantFinder/main.cpp
+++ MatrixDeterminantFinder/main.cpp
@@ -1,13 +1,11 @@
 ﻿#include "matrix.h"
-#include <iostream>
-#include <fstream>
 
 int main()
 {
     //matrix mat("C:\\Users\\User\\Desktop\\matrix.txt");
     setlocale(LC_ALL, "ru");
     matrix mat;
-    //mat.set_from_file("C:\\Users\\User\\Desktop\\matrix.txt");
+    mat.set_from_file("C:\\Users\\User\\Desktop\\matrix.txt");
     mat.show();
     double det = mat.get_det();
     cout << "det = " << det;
